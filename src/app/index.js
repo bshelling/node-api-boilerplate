3e26cf57cb922f19f37785b47542984b3e4aa407
Brandon Shelling <info@bcsgraffixanddesign.com>
2020-05-27 00:14:56 -0500

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import cors from "cors"
+import { json, urlencoded } from "body-parser"
+
+const app = express()
+app.use(urlencoded({ extended: true }))
+app.use(cors())
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello Boilerplate" })
+})
+
+app.listen(3000, () => {
+  console.log("http://localhost:3000")
+})
